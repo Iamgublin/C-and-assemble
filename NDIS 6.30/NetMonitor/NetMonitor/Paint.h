de121789181d78e6b4e587e24fe7db8b6be35f89
Iamgublin <403760189@qq.com>
2017-05-29 20:24:39 +0800

    2017/5/29

--- NDIS 6.30/NetMonitor/NetMonitor/Paint.h
+++
@@ -1,5 +0,0 @@
-#include "global.h"
-#include "ControlApi.h"
-bool Paint(HWND hwnd);
-void InitAttackTreeView(HWND TreeView);
-void InitSelCardTreeView(HWND TreeView);
--- NDIS 6.30/NetMonitor/NetMonitor/global.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Windows.h>
-#include"Ui.h"
-#include"NetMonitor.h"
-#include<CommCtrl.h>
-#include<map>
-#include<vector>
